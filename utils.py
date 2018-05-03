8dfb47f5de7e369f80652472111b8fcbc2504d77
Yang Liu <foamliu@yeah.net>
2018-05-03 18:11:27 +0800

    learn how custom loss work

--- utils.py
+++ utils.py
@@ -2,9 +2,9 @@ import keras.backend as K
 
 
 def matting_loss(y_true, y_pred):
-    print('y_true.shape: ' + str(y_true.shape))
-    print('y_pred.shape: ' + str(y_pred.shape))
     epsilon = 1e-6
     epsilon_sqr = K.constant(epsilon ** 2)
-    return K.mean(K.sqrt(K.square(y_true - y_pred) + epsilon_sqr))
+    fake = K.constant(1)
+    #return K.mean(K.sqrt(K.square(y_pred - y_true) + epsilon_sqr))
+    return fake
 
