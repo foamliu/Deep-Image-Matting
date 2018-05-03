cfe6a8bff3417ddca82fb0026ea778c3e8544745
Yang Liu <foamliu@stcgpu-13.corp.microsoft.com>
2018-05-03 18:18:49 +0800

    revert

--- utils.py
+++ utils.py
@@ -4,7 +4,6 @@ import keras.backend as K
 def matting_loss(y_true, y_pred):
     epsilon = 1e-6
     epsilon_sqr = K.constant(epsilon ** 2)
-    fake = K.constant(1)
-    #return K.mean(K.sqrt(K.square(y_pred - y_true) + epsilon_sqr))
-    return fake
+    return K.mean(K.sqrt(K.square(y_pred - y_true) + epsilon_sqr))
+
 
