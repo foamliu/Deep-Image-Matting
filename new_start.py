f6fe58beb160341c50ae42447cac5579c24ea745
Yang Liu <foamliu@yeah.net>
2018-05-11 13:56:32 +0800

    RecursionError: maximum recursion depth exceeded

--- new_start.py
+++ new_start.py
@@ -1,16 +1,14 @@
 import keras.backend as K
+import tensorflow as tf
 from keras.models import Sequential
+from keras.utils.training_utils import multi_gpu_model
 
 from encoder_decoder import build_encoder, build_decoder
-from utils import do_compile
-from keras.utils.training_utils import multi_gpu_model
-import tensorflow as tf
-
 from utils import do_compile
 from utils import get_available_gpus
 
 
-def autoencoder(img_rows, img_cols, channel=4):
+def do_autoencoder(img_rows, img_cols, channel=4):
     model = Sequential()
     # Encoder
     build_encoder(model, img_rows, img_cols, channel)
@@ -25,10 +23,10 @@ def autoencoder(img_rows, img_cols, channel=4):
     num_gpu = len(get_available_gpus())
     if num_gpu >= 2:
         with tf.device("/cpu:0"):
-            new_model = autoencoder(img_rows, img_cols, channel)
+            new_model = do_autoencoder(img_rows, img_cols, channel)
             new_model = multi_gpu_model(new_model, gpus=num_gpu)
     else:
-        new_model = autoencoder(img_rows, img_cols, channel)
+        new_model = do_autoencoder(img_rows, img_cols, channel)
 
     new_model = do_compile(new_model)
     return new_model
