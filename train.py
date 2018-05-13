ed67496045d8660ad8b1c0400a1c72d6f9ce30de
Yang Liu <foamliu@yeah.net>
2018-05-13 23:48:02 +0800

    stop then finetune

--- train.py
+++ train.py
@@ -8,27 +8,30 @@ from keras.utils import multi_gpu_model
 import migrate
 from config import *
 from data_generator import train_gen, valid_gen
+from new_start import autoencoder
 from trimap_dict import trimap_init, trimap_clear
 from utils import custom_loss, get_available_cpus, get_available_gpus
 
 if __name__ == '__main__':
     # Parse arguments
     ap = argparse.ArgumentParser()
-    ap.add_argument("-t", "--trained", help="path to save trained model files")
+    ap.add_argument("-c", "--checkpoint", help="path to save checkpoint model files")
+    ap.add_argument("-p", "--pretrained", help="path to save pretrained model files")
     args = vars(ap.parse_args())
-    trained_path = args["trained"]
-    if trained_path is None:
-        trained_models_path = 'models/'
+    checkpoint_path = args["checkpoint"]
+    pretrained_path = args["pretrained"]
+    if checkpoint_path is None:
+        checkpoint_models_path = 'models/'
     else:
-        # python train.py -t /mnt/Deep-Image-Matting/models/
-        trained_models_path = '{}/'.format(trained_path)
+        # python train.py -c /mnt/Deep-Image-Matting/models/
+        checkpoint_models_path = '{}/'.format(checkpoint_path)
 
     # Init trimap dict
     trimap_init()
 
     # Callbacks
     tensor_board = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=True)
-    model_names = trained_models_path + 'model.{epoch:02d}-{val_loss:.2f}.hdf5'
+    model_names = checkpoint_models_path + 'model.{epoch:02d}-{val_loss:.4f}.hdf5'
     model_checkpoint = ModelCheckpoint(model_names, monitor='val_loss', verbose=1, save_best_only=True)
     early_stop = EarlyStopping('val_loss', patience=patience)
     reduce_lr = ReduceLROnPlateau('val_loss', factor=0.1, patience=int(patience / 4), verbose=1)
@@ -41,7 +44,7 @@ if __name__ == '__main__':
             self.model_to_save = model
 
         def on_epoch_end(self, epoch, logs=None):
-            fmt = trained_models_path + 'model.%02d-%.2f.hdf5'
+            fmt = checkpoint_models_path + 'model.%02d-%.4f.hdf5'
             self.model_to_save.save(fmt % (epoch, logs['val_loss']))
 
 
@@ -49,15 +52,23 @@ if __name__ == '__main__':
     num_gpu = len(get_available_gpus())
     if num_gpu >= 2:
         with tf.device("/cpu:0"):
-            print("Training with {} GPUs...".format(num_gpu))
-            model = migrate.migrate_model(img_rows, img_cols, channel)
+            if pretrained_path is None:
+                model = migrate.migrate_model(img_rows, img_cols, channel)
+            else:
+                model = autoencoder(img_rows, img_cols, channel)
+                model.load_weights(pretrained_path)
 
         new_model = multi_gpu_model(model, gpus=num_gpu)
         # rewrite the callback: saving through the original model and not the multi-gpu model.
         model_checkpoint = MyCbk(model)
     else:
-        new_model = migrate.migrate_model(img_rows, img_cols, channel)
-    # sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.99, nesterov=True)
+        if pretrained_path is None:
+            new_model = migrate.migrate_model(img_rows, img_cols, channel)
+        else:
+            new_model = autoencoder(img_rows, img_cols, channel)
+            new_model.load_weights(pretrained_path)
+
+    # sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
     new_model.compile(optimizer='nadam', loss=custom_loss)
 
     print(new_model.summary())
@@ -66,7 +77,7 @@ if __name__ == '__main__':
     num_cpu = get_available_cpus()
     workers = int(round(num_cpu / 2))
     print('num_gpu={}\nnum_cpu={}\nworkers={}\ntrained_models_path={}.'.format(num_gpu, num_cpu, workers,
-                                                                               trained_models_path))
+                                                                               checkpoint_models_path))
 
     # Final callbacks
     callbacks = [tensor_board, model_checkpoint, early_stop, reduce_lr, cleanup]
@@ -75,7 +86,7 @@ if __name__ == '__main__':
     new_model.fit_generator(train_gen(),
                             steps_per_epoch=num_train_samples // batch_size,
                             validation_data=valid_gen(),
-                            validation_steps=num_valid_samples / batch_size,
+                            validation_steps=num_valid_samples // batch_size,
                             epochs=epochs,
                             verbose=1,
                             callbacks=callbacks,
--- trimap_dict.py
+++ trimap_dict.py
@@ -39,6 +39,7 @@ if __name__ == '__main__':
     trimap_init()
 
     trimap_add([1, 2, 3], [3, 2, 1])
+    trimap_add([1, 2, 3], [4, 5, 6])
     trimap = trimap_get([1, 2, 3])
     print(trimap)
     trimap = trimap_get([1, 2, 2])
