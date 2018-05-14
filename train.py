f116863bcb202c04307ff3d2b5b25780cf121038
Yang Liu <foamliu@yeah.net>
2018-05-14 16:46:37 +0800

    ts

--- train.py
+++ train.py
@@ -25,24 +25,6 @@ if __name__ == '__main__':
         # python train.py -c /mnt/Deep-Image-Matting/models/
         checkpoint_models_path = '{}/'.format(checkpoint_path)
 
-    # Callbacks
-    tensor_board = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=True)
-    model_names = checkpoint_models_path + 'model.{epoch:02d}-{val_loss:.4f}.hdf5'
-    model_checkpoint = ModelCheckpoint(model_names, monitor='val_loss', verbose=1, save_best_only=True)
-    early_stop = EarlyStopping('val_loss', patience=patience)
-    reduce_lr = ReduceLROnPlateau('val_loss', factor=0.1, patience=int(patience / 4), verbose=1)
-
-
-    class MyCbk(keras.callbacks.Callback):
-        def __init__(self, model):
-            keras.callbacks.Callback.__init__(self)
-            self.model_to_save = model
-
-        def on_epoch_end(self, epoch, logs=None):
-            fmt = checkpoint_models_path + 'model.%02d-%.4f.hdf5'
-            self.model_to_save.save(fmt % (epoch, logs['val_loss']))
-
-
     # Load our model, added support for Multi-GPUs
     num_gpu = len(get_available_gpus())
     if num_gpu >= 2:
@@ -56,7 +38,7 @@ if __name__ == '__main__':
 
         new_model = multi_gpu_model(model, gpus=num_gpu)
         # rewrite the callback: saving through the original model and not the multi-gpu model.
-        model_checkpoint = MyCbk(model)
+        # model_checkpoint = MyCbk(model)
     else:
         if pretrained_path is not None:
             new_model = create_model()
@@ -64,29 +46,3 @@ if __name__ == '__main__':
         else:
             new_model = create_model()
             migrate.migrate_model(new_model)
-
-    # sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
-    new_model.compile(optimizer='nadam', loss=custom_loss_wrapper(new_model.input))
-
-    print(new_model.summary())
-
-    # Summarize then go!
-    num_cpu = get_available_cpus()
-    workers = int(round(num_cpu / 2))
-    print('num_gpu={}\nnum_cpu={}\nworkers={}\ntrained_models_path={}.'.format(num_gpu, num_cpu, workers,
-                                                                               checkpoint_models_path))
-
-    # Final callbacks
-    callbacks = [tensor_board, model_checkpoint, early_stop, reduce_lr]
-
-    # Start Fine-tuning
-    new_model.fit_generator(train_gen(),
-                            steps_per_epoch=num_train_samples // batch_size,
-                            validation_data=valid_gen(),
-                            validation_steps=num_valid_samples // batch_size,
-                            epochs=epochs,
-                            verbose=1,
-                            callbacks=callbacks,
-                            use_multiprocessing=True,
-                            workers=workers
-                            )
