d7e7bd31b3fc75244f87849a682f810a8106e87f
Yang Liu <foamliu@yeah.net>
2018-05-14 16:03:40 +0800

    single GPU version to troubleshoot

--- train.py
+++ train.py
@@ -4,10 +4,9 @@ import keras
 import tensorflow as tf
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 from keras.utils import multi_gpu_model
-from keras.optimizers import SGD
 
 import migrate
-from config import img_rows, img_cols, channel, patience
+from config import img_rows, img_cols, channel, patience, batch_size, epochs, num_train_samples, num_valid_samples
 from data_generator import train_gen, valid_gen
 from model import create_model
 from utils import custom_loss_wrapper, get_available_cpus, get_available_gpus
@@ -45,24 +44,24 @@ if __name__ == '__main__':
 
 
     # Load our model, added support for Multi-GPUs
-    num_gpu = len(get_available_gpus())
-    if num_gpu >= 2:
-        with tf.device("/cpu:0"):
-            if pretrained_path is None:
-                model = migrate.migrate_model(img_rows, img_cols, channel)
-            else:
-                model = create_model(img_rows, img_cols, channel)
-                model.load_weights(pretrained_path)
-
-        new_model = multi_gpu_model(model, gpus=num_gpu)
-        # rewrite the callback: saving through the original model and not the multi-gpu model.
-        model_checkpoint = MyCbk(model)
+    # num_gpu = len(get_available_gpus())
+    # if num_gpu >= 2:
+    #     with tf.device("/cpu:0"):
+    #         if pretrained_path is None:
+    #             model = migrate.migrate_model(img_rows, img_cols, channel)
+    #         else:
+    #             model = create_model(img_rows, img_cols, channel)
+    #             model.load_weights(pretrained_path)
+    #
+    #     new_model = multi_gpu_model(model, gpus=num_gpu)
+    #     # rewrite the callback: saving through the original model and not the multi-gpu model.
+    #     model_checkpoint = MyCbk(model)
+    # else:
+    if pretrained_path is None:
+        new_model = migrate.migrate_model(img_rows, img_cols, channel)
     else:
-        if pretrained_path is None:
-            new_model = migrate.migrate_model(img_rows, img_cols, channel)
-        else:
-            new_model = create_model(img_rows, img_cols, channel)
-            new_model.load_weights(pretrained_path)
+        new_model = create_model(img_rows, img_cols, channel)
+        new_model.load_weights(pretrained_path)
 
     # sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
     new_model.compile(optimizer='nadam', loss=custom_loss_wrapper(new_model.input))
