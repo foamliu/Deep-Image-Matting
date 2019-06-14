512a5aa727140e2cb5259b088693d789708db835
Yang Liu <liuyang12@focusmedia.cn>
2019-06-14 09:38:14 +0800

    update

--- data_generator.py
+++ data_generator.py
@@ -14,13 +14,13 @@ from config import unknown_code
 from utils import safe_crop
 
 kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3))
-with open('Combined_Dataset/Training_set/training_fg_names.txt') as f:
+with open('data/Combined_Dataset/Training_set/training_fg_names.txt') as f:
     fg_files = f.read().splitlines()
-with open('Combined_Dataset/Test_set/test_fg_names.txt') as f:
+with open('data/Combined_Dataset/Test_set/test_fg_names.txt') as f:
     fg_test_files = f.read().splitlines()
-with open('Combined_Dataset/Training_set/training_bg_names.txt') as f:
+with open('data/Combined_Dataset/Training_set/training_bg_names.txt') as f:
     bg_files = f.read().splitlines()
-with open('Combined_Dataset/Test_set/test_bg_names.txt') as f:
+with open('data/Combined_Dataset/Test_set/test_bg_names.txt') as f:
     bg_test_files = f.read().splitlines()
 
 
